6cd0a869e726a0c240cc29172c4e4d8bd8d76561
Charles Kenney <Charlesc.kenney@gmail.com>
2017-10-03 10:38:11 -0400

    Initial Commit

---
+++ to-do-list/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  to-do-list
+//
+//  Created by Charles Kenney on 10/3/17.
+//  Copyright © 2017 Charles Kenney. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ to-do-listTests/to_do_listTests.swift
@@ -0,0 +1,36 @@
+//
+//  to_do_listTests.swift
+//  to-do-listTests
+//
+//  Created by Charles Kenney on 10/3/17.
+//  Copyright © 2017 Charles Kenney. All rights reserved.
+//
+
+import XCTest
+@testable import to_do_list
+
+class to_do_listTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
